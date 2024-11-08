b476f01d33dc2fa1ac230854a792cd9130a3dacd
NathanKassahun1234 <Kassahunny@uchicago.edu>
2024-11-08 18:25:40 +0000

    adds movie create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,18 @@
 class MoviesController < ApplicationController
+  def create
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")  # Fixed this line
+  end
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
@@ -8,7 +22,6 @@ class MoviesController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-
     matching_movies = Movie.where({ :id => the_id })
     @the_movie = matching_movies.at(0)
 
